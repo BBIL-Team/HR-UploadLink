f3b3c2465d92fa9c9a5f5bf8db22bc3ed982f964
BBIL-Team <deepshika5686@bharatbiotech.com>
2025-09-29 11:50:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -114,7 +114,7 @@ const App: React.FC = () => {
   };
 
   return (
-    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '90vw', backgroundColor: 'teal' }}>
+    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '90vw', backgroundColor: '#f8f8ff' }}>
       <header style={{ width: '100%' }}>
         <div style={{ width: '130px', height: '90px', overflow: 'hidden', borderRadius: '8px' }}>
           <img
